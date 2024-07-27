4e91218e4b3e15aa1753d8ebaa8996ada4320e9c
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-07-27 11:47:19 +0530

    Add files via upload

---
+++ AddTwoNumWithoutConsole.java
@@ -0,0 +1,8 @@
+public class AddTwoNumWithoutConsole{
+    public static void main(String[] args){
+        int a=12,b=3;
+        int sum=0;
+        sum=a+b;
+        System.out.println("The sum of "+a+"+"+b+" is "+sum);
+    }
+}
